85b771baa30d255fc0d51a8ab971ad905daaa8c0
Raghav Chandra <raghav.yo@gmail.com>
2017-10-02 18:41:40 +0000

    Removing all

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-class AppMain extends React.Component {
-render() {
-return (<div>Raghav Chandra</div>);
-}
-
-render(<AppMain/>,  document.getElementById("app"));
