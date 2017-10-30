f434148a52fc1f654da551c8f4a4b789f0a12aa6
Raghav Chandra <raghav.yo@gmail.com>
2017-10-30 19:07:11 +0000

    button positioning

--- app/usecaseList.js
+++ app/usecaseList.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {CardHeader,CardFooter, Card,CardBody, Button, CardTitle, CardText, Row, Col,CardSubtitle } from 'reactstrap';
+import {CardHeader,CardFooter, Card,CardBody, Button,ButtonGroup, CardTitle, CardText, Row, Col,CardSubtitle } from 'reactstrap';
 import {USER_ACTION} from './constants'
 import {execute} from './network'
 
+const styles = {
+	bottomButtons:{
+		position:'absolute',
+		bottom:0,
+		right:0
+	},
+	topButtons:{
+		position:'absolute',
+                top:0,
+                right:0
+	}
+};
 export class UseCaseCard extends React.Component{
 	constructor(props){
 		super(props);
@@ -11,14 +23,12 @@ export class UseCaseCard extends React.Component{
 	render() {
 		  return (<div class='pull-left' style={{marginRight:'10px'}}>
 			<Card body="true" inverse style={{ backgroundColor: '#333', borderColor: '#333',height:'180px',width:'300px' }} >
-			        <CardTitle>{this.props.title}
-					<div className='pull-right'>
-					<Button color="danger"><i class="fa fa-trash-o fa-1"></i></Button>
-					<Button color="default"><i class="fa fa-pencil fa-fw fa-1"></i></Button>
-					<Button color="success"><i class="fa fa-play fa-1"></i></Button>
-					</div>
-				</CardTitle>
-			        <CardText>{this.props.summary}</CardText>
+			        <CardTitle>{this.props.title}<Button color="primary" style={styles.topButtons} ><i class="fa fa-play fa-2"></i></Button></CardTitle>
+			       	<CardText>{this.props.summary}</CardText>
+				<ButtonGroup size='sm'  style={styles.bottomButtons}>
+                                <Button color="danger" ><i class="fa fa-trash-o fa-2"></i></Button>
+                                <Button color="default" ><i class="fa fa-pencil fa-fw fa-2"></i></Button>
+				</ButtonGroup>
 		        </Card>
 		</div>);
 	}
