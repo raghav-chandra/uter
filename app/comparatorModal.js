fbc5f34f47118e49d5f5772d6d56c5e561e68da4
Raghav Chandra <raghav.yo@gmail.com>
2019-03-19 02:05:13 +0530

    Comparison of data

--- app/comparatorModal.js
+++ app/comparatorModal.js
@@ -7,47 +7,85 @@ import {launchComparatorModal} from './redux/action';
 export class Comparator extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {open: false, title: '', data: {}};
+        this.state = {open: false, title: this.props.title, nestedOpen: false, nestedData: {}};
+        this.toggleNested = this.toggleNested.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({open: nextProps.open, act: nextProps.act, diff: nextProps.diff, exp: nextProps.exp, title: nextProps.title});
+    }
+
+    toggleNested(nestedData) {
+        this.setState({nestedData, nestedOpen: !this.state.nestedOpen})
     }
 
     render() {
         let body = [];
+        let comparator = (<div/>);
         if(this.props.open){
-            let data = this.props.data;
-            let expected = JSON.parse(data.uc.expected);
-            Object.keys(data.matching).forEach(key => {
-                let tds = [];
-                tds.push(<td><b>{key}</b></td>)
-                tds.push(<td><b>{expected[key]}</b></td>);
-                tds.push(<td
-                    style={data.matching[key] === 'MATCHING' ? {} : {backgroundColor: 'red'}}>{data.actual[key]}</td>);
-                body.push(<tr key={key}>{tds}</tr>);
-            });
+            let expected = this.state.exp;
+            let diff = this.state.diff;
+
+            if(expected) {
+                Object.keys(expected).forEach(key => {
+                    let tds = [];
+                    tds.push(<td><b>{key}</b></td>)
+                    let expObjType = null;
+                    if(expected[key] && expected[key].constructor === Array){
+                        expObjType = 'Array';
+                        tds.push(<td>{expObjType}</td>);
+                    } else if (expected[key] && typeof expected[key] === 'object') {
+                        expObjType = 'Object';
+                        tds.push(<td>{expObjType}</td>);
+                    } else {
+                        tds.push(<td>{expected[key]}</td>);
+                    }
+
+                    let nestedObj = diff.status === 'P' || diff[key].status === 'P'
+                                    ? {act: this.state.act[key], exp: expected[key], diff: {status: 'P'}} : diff[key];
+                    nestedObj.title = 'Comparison for : ' + key;
+
+                    tds.push(<td style={diff.status === 'P' || diff[key].status === 'P' ? {} : {backgroundColor: 'red'}}
+                                onClick = {() => expObjType ? this.toggleNested(nestedObj) : null}>
+                        {expObjType ? expObjType : this.state.act[key]}</td>);
+
+                    body.push(<tr key={key}>{tds}</tr>);
+                });
+            }
+            comparator = (<Comparator
+                                open = {this.state.nestedOpen}
+                                act = {this.state.nestedData.act}
+                                exp = {this.state.nestedData.exp}
+                                diff = {this.state.nestedData.diff}
+                                title = {this.state.nestedData.title}
+                                closePopup = {() => this.toggleNested({})}/>);
         }
 
         return (<div>
-            <Modal show={this.props.open} onHide={this.props.closePopup}>
-                <Modal.Header classButton>
-                    <Modal.Title>{this.props.title}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Table>
-                        <thead style={{fontWeight: 'bold'}}>
-                            <tr>
-                                <td>Attributes</td>
-                                <td>Expected</td>
-                                <td>Actual</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {body}
-                        </tbody>
-                    </Table>
-                <Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={this.handleClose}>Close</Button>
-                </Modal.Footer>
-        );
+                {comparator}
+                <Modal show={this.props.open} onHide={this.props.closePopup}>
+                    <Modal.Header classButton>
+                        <Modal.Title>{this.props.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Table>
+                            <thead style={{fontWeight: 'bold'}}>
+                                <tr>
+                                    <td>Attributes</td>
+                                    <td>Expected</td>
+                                    <td>Actual</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {body}
+                            </tbody>
+                        </Table>
+                    <Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.props.closePopup}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>);
     }
 }
 
@@ -58,7 +96,10 @@ const mapDispatchToProps = dispatch => {
 const mapStateToProps = state => {
     return {
         open: state.launchComparatorModal.open,
-        data: state.launchComparatorModal.data
+        diff: state.launchComparatorModal.data.diff,
+        act: state.launchComparatorModal.data.act,
+        exp: state.launchComparatorModal.data.uc && JSON.parse(state.launchComparatorModal.data.uc.expected),
+        title: state.launchComparatorModal.data.uc && state.launchComparatorModal.data.uc.id + ':' + state.launchComparatorModal.data.uc.summary,
     };
 };
 
